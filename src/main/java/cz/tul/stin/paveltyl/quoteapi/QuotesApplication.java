c26d8136d541ae2b9f317bc885d5a1996dd30a55
Pavel Tyl <pavel.tyl@gmail.com>
2026-03-25 19:47:08 +0100

    init: create Spring Boot project with Spring Web and Lombok

---
+++ src/main/java/cz/tul/stin/paveltyl/quoteapi/QuotesApplication.java
@@ -0,0 +1,13 @@
+package cz.tul.stin.paveltyl.quoteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/tul/stin/paveltyl/quoteapi/QuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tul.stin.paveltyl.quoteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
