8db160456f4c0dd139ee1592ad802611dd57ae2d
Pavel Tyl <pavel.tyl@gmail.com>
2026-03-25 22:24:54 +0100

    feat: add RestTemplate bean

--- src/main/java/cz/tul/stin/paveltyl/quoteapi/QuotesApplication.java
+++ src/main/java/cz/tul/stin/paveltyl/quoteapi/QuotesApplication.java
@@ -3,11 +3,19 @@ package cz.tul.stin.paveltyl.quoteapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
 @SpringBootApplication
 public class QuotesApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(QuotesApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() { // = nástroj pro HTTP volání
+        return new RestTemplate();
+    }
 }
